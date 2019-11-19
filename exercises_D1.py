d451de559a7b0e51198cd9b3f614a566481802b0
naaman94 <51744606+naaman94@users.noreply.github.com>
2019-11-19 15:17:25 +0200

    first day exercises
    input , print , variable types and operations in python

---
+++ exercises_D1.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 12:28:28 2019
+
+@author: owner
+"""
+
+#exercise 1
+"""
+print("Hello")
+print("\t Hello")
+print("\t \t Hello")
+"""
+#exercise 2
+"""
+print("OrangeAcademy \n"*20)
+"""
+#exercise 3
+"""
+x=float(1)
+y=float(2.8)
+z=float("3")
+w=float("4.2")
+print(x,y,z,w,sep='10')
+"""
+#exercise 4
+"""
+num1=float(input("Inter The 1st Number: "))
+num2=float(input("Inter The 2nd Number: "))
+print("The total is :",num1*num2)
+"""
+#exercise 5
+"""
+num1=float(input("Inter The 1st Number: "))
+num2=float(input("Inter The 2nd Number: "))
+num3=float(input("Inter The 3rd Number: "))
+num4=float(input("Inter The 4th Number: "))
+num5=float(input("Inter The 5th Number: "))
+print("The avr is :",(num1+num2+num3+num4+num5)/5)
+"""
+#exercise 6
+"""
+x =1
+y =2.8 
+z=1j
+o="Orange"
+A= True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
+print(type(A))
+"""
+#exercise 7
+"""
+x,y="orange",1
+X1,y1=100,-10
+print(x)
+print(y)
+print(x1)#it should be X1
+print(y1)
+"""
+#exercise 8
+
+#5x=10
+#print(5x)
+#x5=10
+#print(x5)
+#X_q$="orange"
+#it should have only alpha-num and underscore
+#print(X_q$)
+#A B =14
+#print(A B)
+#it should have only alpha-num and underscore
+#print("10"*10)
+#the answer is 10101010101010101010
+#print(10*10)
+#the answer is 100
+#print(int("10")*10)
+#the answer is 100
+
+
+#exercise 9
+#exercise 10
+
+#exercise 11
+"""
+#print("hello, world")
+print("Cheers,Mate!)#this is the program
+'''test
+the code
+'''
+"""
+#exercise 12
+'''
+name=input("Inter Your name: ")
+age=int(input("Inter Your Age: "))
+import datetime
+x = datetime.datetime.now().year
+print("Hello ",name,"," ,"You will be 100 Years in ", x+100-age)
+'''
+#exercise 13
+'''
+base=float(input("Inter the base of Triangle: "))
+height=float(input("Inter the height of Triangle: "))
+area=0.5*base*height 
+print("The area of triangle ",area)
+'''
+#exercise 14
+"""
+x=11
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(abs(x*-1))
+print(int(x))
+print(float(x))
+print(divmod(x,y))
+print(pow(x,y))
+print(x**y)
+print(x>y)
+print(x==y)
+print(x!=y)
+"""
+
+
+
